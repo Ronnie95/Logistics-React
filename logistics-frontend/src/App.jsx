79d4b1ba6322037feca199c359332b981ece855d
Ronnie95 <t_daron@yahoo.com>
2026-07-07 00:14:08 -0400

    updates

--- logistics-frontend/src/App.jsx
+++ logistics-frontend/src/App.jsx
@@ -8,6 +8,7 @@ import CustomersCreate from "./components/CustomersCreate"
 import Trucks from "./components/Trucks"
 import TruckCreate from "./components/TrucksCreate"
 import TruckEdit from "./components/TrucksEdit"
+import TruckDetail from "./components/TruckShow"
 
 
 function App() {
@@ -15,23 +16,29 @@ function App() {
     <div>
 
 
-  <Routes>
+      <Routes>
 
-  <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<Register />} />
 
-  <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
 
-  <Route path="/customers" element={<Customers />} />
+      <Route path="/customers" element={<Customers />} />
 
-  <Route path="/customers/new" element={<CustomersCreate />} />
+      <Route path="/customers/new" element={<CustomersCreate />} />
 
-  <Route path="/customers/:id" element={<CustomerDetail />} />
+      <Route path="/customers/:id" element={<CustomerDetail />} />
 
-  <Route path="/customers/:id/edit" element={<CustomerEdit />} />
+      <Route path="/customers/:id/edit" element={<CustomerEdit />} />
 
+      <Route path="/trucks" element={<Trucks />} />
 
+      <Route path="/trucks/new" element={<TruckCreate />} />
 
-  </Routes>
+      <Route path="/trucks/:id" element={<TruckDetail />} />
+
+      <Route path="/trucks/:id/edit" element={<TruckEdit />} />
+
+      </Routes>
 
 
     </div>
