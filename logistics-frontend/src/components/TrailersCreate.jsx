10387049fb72b5c50977b588f33b2995ce5ecc9d
Ronnie95 <t_daron@yahoo.com>
2026-07-07 00:23:07 -0400

    trailer create

---
+++ logistics-frontend/src/components/TrailersCreate.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import { Form, Button, Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
+
+
+function TrailerCreate() {
+
+    const navigate = useNavigate();
+
+    const [trailerForm, setTrailerForm] = useState({
+        trailer_number: "",
+        trailer_options: ""
+    });
+
+
+    const handleChange = (e) => {
+
+        setTrailerForm({
+            ...trailerForm,
+            [e.target.name]: e.target.value
+        });
+
+    };
+
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+
+        try {
+
+            await api.post("trailers/", trailerForm);
+
+            navigate("/trailers");
+
+        } catch (error) {
+
+            console.log(error.response?.data || error.message);
+
+        }
+
+    };
+
+
+    return (
+
+        <div className="container mt-4">
+
+            <Card>
+
+                <Card.Body>
+
+                    <Card.Title>
+                        Create Trailer
+                    </Card.Title>
+
+
+                    <Form onSubmit={handleSubmit}>
+
+
+                        <Form.Group className="mb-3">
+
+                            <Form.Label>
+                                Trailer Number
+                            </Form.Label>
+
+                            <Form.Control
+                                type="integer"
+                                name="trailer_number"
+                                value={truckForm.truck_number}
+                                onChange={handleChange}
+                                required
+                            />
+
+                        </Form.Group>
+
+
+                        <Form.Select
+                                name="trailer_options"
+                                value={trailerForm.trailer_options}
+                                onChange={handleChange}
+                                required
+                            >
+
+                                <option value="">
+                                    Select Trailer
+                                </option>
+
+                                <option value="not_complete">
+                                    28 FT
+                                </option>
+
+                                <option value="in_progress">
+                                    53 FT
+                                </option>
+
+
+                            </Form.Select>
+
+                        
+                        <Button 
+                            variant="primary" 
+                            type="submit"
+                        >
+                            Create Trailer
+                        </Button>
+
+
+                    </Form>
+
+
+                </Card.Body>
+
+            </Card>
+
+        </div>
+
+    );
+
+}
+
+
+export default TrailerCreate;
