32a78507d908b0ce0bb7c5d91068d4270a23dea3
Ronnie95 <t_daron@yahoo.com>
2026-07-08 00:26:15 -0400

    route creation

---
+++ logistics-frontend/src/components/Routes.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
+import api from "../services/api";
+
+function Routes() {
+
+    const [routes, setRoutes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+
+        const getRoutes = async () => {
+
+            try {
+
+                const response = await api.get("routes/");
+
+                setTickets(response.data);
+
+            } catch (err) {
+
+                setError("Unable to load routes.");
+
+                console.log(err);
+
+            } finally {
+
+                setLoading(false);
+
+            }
+
+        };
+
+        getTickets();
+
+    }, []);
+
+    if (loading) {
+        return <h3>Loading Routes...</h3>;
+    }
+
+    if (error) {
+        return <h3>{error}</h3>;
+    }
+
+    return (
+
+        <div className="container mt-4">
+
+            <div className="d-flex justify-content-between mb-4">
+
+                <h2>Routes</h2>
+
+                <Link to="/tickets/new">
+                    <Button>Create a Route</Button>
+                </Link>
+
+            </div>
+
+            {routes.length === 0 ? (
+
+                <p>No Routes found.</p>
+
+            ) : (
+
+                routes.map((route) => (
+
+                    <Card
+                        className="mb-3"
+                        key={route.id}
+                    >
+
+                        <Card.Body>
+
+                            <Card.Title>
+                                {route.route_name}
+                            </Card.Title>
+
+                            <Card.Text>
+                                {route.route_date}
+                            </Card.Text>
+
+
+                            <Link to={`/routes/${route.id}`}>
+
+                                <Button
+                                    variant="outline-primary"
+                                >
+                                    View Route
+                                </Button>
+
+                            </Link>
+
+                        </Card.Body>
+
+                    </Card>
+
+                ))
+
+            )}
+
+        </div>
+
+    );
+
+}
+
+export default Routes;
